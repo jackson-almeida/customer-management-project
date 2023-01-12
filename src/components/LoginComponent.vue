b127a8c93d761bd3a4dd25a2800d95de80d694e8
Jackson de Almeida <jacksonalmeida@tokenlab.com.br>
2023-01-11 22:28:32 -0300

    Added bootstrap and implementing login page with routing

---
+++ src/components/LoginComponent.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+  defineProps<{
+    title: string
+  }>()
+</script>
+
+<template>
+  <form class="p-4 w-100">
+    <h2>{{ title }}</h2>
+    <div class="form-group mb-4">
+      <label class="text-dark">Insira seu E-mail</label>
+      <input type="email" class="form-control" placeholder="E-mail">
+    </div>
+    <div class="form-group mb-4">
+      <label class="text-dark">Insira sua senha</label>
+      <input type="password" class="form-control" placeholder="Password">
+      <small class="form-text text-muted">Nunca compartilhe sua senha com terceiros!</small>
+    </div>
+    <button type="submit" class="btn btn-success w-100">
+      <RouterLink to="/about">Entrar</RouterLink>
+    </button>
+  </form>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Permanent+Marker&family=Rampart+One&display=swap');
+
+.content--main {
+  height: 100vh;
+  overflow: hidden;
+}
+
+.bg--img {
+  width: 100%;
+  height: 100%;
+  background-image: url('../../../../assets/images/exemplo_01.png');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+}
+
+.title{
+  font-family: 'Rampart One', cursive;
+  font-size: 50px;
+}
+
+.subtitle {
+  font-size: 25px;
+}
+
+
+@media(max-width: 770px) {
+  .content-img {
+    display: none !important;
+  }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
 
 import App from './App.vue'
 import router from './router'
+// import bootstrap from './bootstrap.js'
 
 import './assets/main.css'
 
@@ -10,5 +13,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(bootstrap)
 
 app.mount('#app')
